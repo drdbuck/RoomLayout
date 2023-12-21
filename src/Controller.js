0a4896b655d8d8a1921f6e7b86111cd1755214ad
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-12-21 17:41:49 -0500

    Add arrow keys to controls

--- src/Controller.js
+++ src/Controller.js
@@ -9,15 +9,19 @@ class Controller {
     processInput(event) {
         switch (event.keyCode) {
             case 87://W key
+            case 38://Up Arrow
                 this.camera.position.z -= this.speed;
                 break;
             case 65://A key
+            case 37://Left Arrow
                 this.camera.position.x -= this.speed;
                 break;
             case 83://S key
+            case 40://Up Arrow
                 this.camera.position.z += this.speed;
                 break;
             case 68://D key
+            case 39://Right Arrow
                 this.camera.position.x += this.speed;
                 break;
             default: break;
