af172b586d6841dff6834661b977fa2020bc14e9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-12-21 18:36:45 -0500

    Allow moving objects

--- src/Controller.js
+++ src/Controller.js
@@ -36,15 +36,49 @@ class Controller {
         //2023-12-21: copied from https://discourse.threejs.org/t/how-can-i-get-the-position-of-mouse-click-point/16864/3
         this.mouse.x = event.clientX / window.innerWidth * 2 - 1;
         this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-        console.log("mouse", this.mouse);
+    }
+
+    processMouseDown(event) {
+        this.processMouseInput(event);
+        this.mouse.down = true;
+        this.origMouse = this.copy(this.mouse);
         this.selectObject();
+    }
+
+    processMouseMove(event) {
+        if (this.mouse.down) {
+            this.processMouseInput(event);
+            if (this.select) {
+                this.moveObject();
+            }
+        }
+    }
+
+    processMouseUp(event) {
+        this.processMouseInput(event);
+        this.mouse.down = false;
+        this.select = undefined;
     }
 
     selectObject() {
         //2023-12-21: copied from https://stackoverflow.com/a/30871007/2336212
-        this.raycaster.setFromCamera(this.mouse, this.camera); 
+        this.raycaster.setFromCamera(this.mouse, this.camera);
         this.objects = this.raycaster.intersectObjects(this.scene.children);
         this.select = this.objects.find(o => o.object.userData.selectable)?.object;
+        if (this.select) {
+            this.origPos = this.copy(this.select.position);
+        }
+    }
+
+    moveObject() {
+        let x = this.origPos.x * this.mouse.x / this.origMouse.x;
+        let z = this.origPos.z * this.mouse.y / this.origMouse.y;
+        this.select.position.x = x;
+        this.select.position.z = z;
+    }
+
+    copy(obj) {
+        return JSON.parse(JSON.stringify(obj));
     }
 }
 export { Controller };
