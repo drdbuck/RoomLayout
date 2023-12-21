0045c50a92969d8552888cba0285304be33fdfe2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-12-21 18:16:10 -0500

    Allow clicking an object

--- src/Controller.js
+++ src/Controller.js
@@ -1,9 +1,13 @@
 "use strict";
+import { Raycaster } from "../js/three.module.js";
 
 class Controller {
-    constructor(camera) {
+    constructor(camera, scene) {
         this.camera = camera;
+        this.scene = scene;
         this.speed = 1;
+        this.mouse = {};
+        this.raycaster = new Raycaster();
     }
 
     processInput(event) {
@@ -26,6 +30,17 @@ class Controller {
                 break;
             default: break;
         }
+    }
+
+    processMouseInput(event) {
+        //2023-12-21: copied from https://discourse.threejs.org/t/how-can-i-get-the-position-of-mouse-click-point/16864/3
+        this.mouse.x = event.clientX / window.innerWidth * 2 - 1;
+        this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        console.log("mouse", this.mouse);
+        //2023-12-21: copied from https://stackoverflow.com/a/30871007/2336212
+        this.raycaster.setFromCamera(this.mouse, this.camera); 
+        let objects = this.raycaster.intersectObjects(this.scene.children);
+        console.log("objects", objects);
     }
 }
 export { Controller };
