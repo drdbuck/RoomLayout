e32ffbbd57422e9ebafe839ee77f9b1cdb478637
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-12-28 18:04:45 -0500

    Refactor Input.js

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,9 @@
 "use strict";
 
 class Input {
-    constructor(camera, scene) {
+    constructor() {
         this.mouse = {
-            state: {},
+            move: new Delegate(),
             down: new Delegate(),
             hold: new Delegate(),
             up: new Delegate(),
@@ -13,94 +13,66 @@ class Input {
             hold: new Delegate(),
             up: new Delegate(),
         };
-    }
-
-    processInput(event) {
-        this.key.down.run(event);
+        this.state = {
+            mouse: {
+                lmbDown: false,
+                //rmbDown: false,
+                //buttons: [],
+                pos: new Vector2(),
+                posStart: new Vector2(),
+            },
+            keys: [],
+        }
     }
 
     processMouseInput(event) {
         //2023-12-21: copied from https://discourse.threejs.org/t/how-can-i-get-the-position-of-mouse-click-point/16864/3
-        this.mouse.state.x = event.clientX / window.innerWidth * 2 - 1;
-        this.mouse.state.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    }
-
-    processMouseDown(event) {
-        this.processMouseInput(event);
-        this.mouse.state.down = true;
-        this.origMouse = copy(this.mouse.state);
-        this.selectObject();
+        this.state.mouse.pos.x = event.clientX / window.innerWidth * 2 - 1;
+        this.state.mouse.pos.y = -(event.clientY / window.innerHeight) * 2 + 1;
     }
 
     processMouseMove(event) {
         this.processMouseInput(event);
-        if (this.mouse.state.down) {
-            // console.log("mouse", this.mouse.state);
-            if (this.select) {
-                this.moveObject();
-            }
-        }
-        else {
-            this.mouse.state.over = this.getObjectAtMousePos();
-            if (this.mouse.state.over) {
-                this.changeCursor("move");
-            }
-            else {
-                this.changeCursor("auto");
-            }
-        }
+        this.mouse.move.run(this.state, event);
+    }
+
+    processMouseDown(event) {
+        this.processMouseInput(event);
+        this.state.mouse.lmbDown = true;
+        this.state.mouse.posStart = this.state.mouse.pos.copy();
+        this.mouse.down.run(this.state, event);
+    }
+    
+    processMouseHold(event) {
+        this.processMouseInput(event);
+        this.mouse.hold.run(this.state, event);
     }
 
     processMouseUp(event) {
-        this.mouse.state.down = false;
-        this.select = undefined;
+        this.state.mouse.lmbDown = false;
+        this.mouse.up.run(this.state, event);
     }
 
-    getObjectAtMousePos() {
-        //2023-12-21: copied from https://stackoverflow.com/a/30871007/2336212
-        this.raycaster.setFromCamera(this.mouse.state, this.camera);
-        let objects = this.raycaster.intersectObjects(this.scene.children);
-        return objects.find(o => o.object.userData.selectable)?.object;
-    }
 
-    selectObject() {
-        this.select = this.getObjectAtMousePos();
-        if (this.select) {
-            this.origPos = new Vector3(this.select.position);
-            this.selectOffset = this.origPos.sub(this.getMouseWorld(this.mouse.state));
+
+    processKeyDown(event) {
+        if (!this.state.keys.includes(event.keyCode)) {
+            this.state.keys.push(event.keyCode);
         }
+        //
+        this.key.down.run(this.state, event);
     }
 
-    moveObject() {
-        let mouseWorld = this.getMouseWorld(this.mouse.state);
-        this.select.position.copy(mouseWorld.add(this.selectOffset));
+    processKeyHold(event) {
+        this.key.hold.run(this.state, event);
     }
 
-    getMouseWorld(mouse) {
-        //2023-12-21: copied from https://stackoverflow.com/a/13091694/2336212
-        var vec = new Vector3();
-        var pos = new Vector3();
-
-        vec.set(
-            mouse.state.x,
-            mouse.state.y,
-            0.5,
-        );
-
-        vec.unproject(this.camera);
-
-        vec.sub(this.camera.position).normalize();
-
-        var distance = - this.camera.position.y / vec.y;
-
-        vec = vec.multiplyScalar(distance);
-
-        pos.copy(this.camera.position).add(vec);
-
-        return pos;
-    }
-
-    changeCursor(cursorStyle) {
-        $("cvsDisplay").style.cursor = cursorStyle;
+    processKeyUp(event) {
+        let index = this.state.keys.indexOf(event.keyCode);
+        if (index >= 0) {
+            this.state.keys.splice(index, 1);
+        }
+        //
+        this.key.up.run(this.state, event);
     }
 }
