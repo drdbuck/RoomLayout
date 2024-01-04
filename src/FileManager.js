4619021dbcd6f068e75e8299e7b05615a65e3882
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-04 18:38:43 -0500

    Customize FileManager

--- src/FileManager.js
+++ src/FileManager.js
@@ -1,90 +1,97 @@
 "use strict";
-
 //2024-01-04: copied from CalendarJournal
-class FileManager{
-    constructor() {
+
+const imageFileTypes = [
+    "image/png",
+];
+const textFileTypes = [
+    "text/plain",
+    "application/json",
+];
+
+class FileManager {
+    constructor(dropPanel) {
+        this.dropPanel = dropPanel;
+
+        //bind responder functions
+        this.preventDefaults.bind(this);
+        this.handleDrop.bind(this);
+        
         //Drop image event handlers// Prevent default drag behaviors
         //2022-05-26: copied from https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
-        let pnlEntry = $("pnlEntry");
         ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-            pnlEntry.addEventListener(eventName, preventDefaults, false);
-            document.body.addEventListener(eventName, preventDefaults, false);
+            dropPanel.addEventListener(eventName, this.preventDefaults, false);
+            document.body.addEventListener(eventName, this.preventDefaults, false);
         })
-        pnlEntry.addEventListener('drop', handleDrop, false);
-        //
+        dropPanel.addEventListener('drop', this.handleDrop, false);
+        
+        //Delegate initialization
+        this.onImageUploaded = new Delegate();//param: image
+        this.onJsonUploaded = new Delegate();//param: json
     }
 
-    function preventDefaults (e) {
+    preventDefaults(e) {
         //2022-05-26: copied from https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
         e.preventDefault()
         e.stopPropagation()
     }
-    function handleDrop(e) {
+    handleDrop(e) {
         //2022-05-26: copied from https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
         let dt = e.dataTransfer;
         let files = dt.files;
         handleFiles(files);
     }
-    const imageFileTypes = [
-        "image/png",
-    ];
-    const symbolSetFileTypes = [
-        "text/plain",
-        "application/json",
-    ];
-    function handleFiles(files){
+  
+    handleFiles(files) {
         //2022-05-26: copied from https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
         files = [...files];
-        files.forEach((file)=>{
-            if (imageFileTypes.includes(file.type)){
-                uploadSymbol(file);
+        files.forEach((file) => {
+            if (imageFileTypes.includes(file.type)) {
+                this.onImageUploaded.run(file);
             }
-            else if (symbolSetFileTypes.includes(file.type)){
+            else if (textFileTypes.includes(file.type)) {
                 handleTextFile(file);
             }
-            else{
-                console.warning("Unknown file type:",file.type,"filename:",file.name);
+            else {
+                console.warning("Unknown file type:", file.type, "filename:", file.name);
             }
         });
     }
-    
-    function handleTextFile(file){
-        if (file.name.endsWith(".entry.json")){
-            uploadEntryList(file);
-        }
-        else {
-            uploadSymbolSet(file);
-        }
-    }
 
-    function uploadSymbol(file){
+    uploadImage(file){
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = function() {
             //Get values
-            let symbolName = file.name.split(".")[0];
+            let imageName = file.name.split(".")[0];
             //Create object
-            let symbol = {
-                name: symbolName,
+            let image = {
+                name: imageName,
                 imageURL: reader.result,
                 icon: new Image(),
             }
-            symbol.icon.src = symbol.imageURL;
-            //Add symbol to symbolSet
-            miscSymbolSet.addSymbol(symbol);
-            //Update UI
-            miscSymbolSet.alphabetizeSymbols();
-            refreshSymbolBank();
-            updateSymbolBank();
+            image.icon.src = image.imageURL;
+            //Run delegate
+            this.onImageUploaded.run(image);
+        }
+    }
+    
+    handleTextFile(file) {
+        if (file.name.endsWith(".json")){
+            uploadJson(file);
+        }
+        else{
+            console.warn("Not implemented: handling text file:", file.name, file.type);
         }
     }
 
-    function uploadSymbolSet(file){
+    uploadJson(file){
         let reader = new FileReader();
         reader.readAsText(file);
         reader.onloadend = function() {
             let json = reader.result;
-            importSymbolSet(json);
+            //Run delegate
+            this.onJsonUploaded.run(json);
         }
     }
 }
