415f95e3103cd2ba9b2b34e7a31dc528eee8d9bc
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-06 22:32:46 -0500

    Add Furniture.js

---
+++ src/Furniture.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Furniture extends Block{
+    constructor(imageURL, width, length, height) {
+        super(width, length, height);
+
+        this.imageURL = imageURL;
+        this.image = undefined;//
+    }
+}
