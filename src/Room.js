abe95a8e87d2da264cab8248c0a21adc92760315
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-06 23:16:07 -0500

    Room has furniture

--- src/Room.js
+++ src/Room.js
@@ -1,7 +1,25 @@
 "use strict";
 
 class Room extends Block{
-    constructor() {
-        super();
+    constructor(width, length, height) {
+        super(width, length, height);
+
+        this.furnitures = [];
+
+        this.onFurnituresChanged = new Delegates("furnitures");
+    }
+
+    addFurniture(furniture) {
+        if (!this.furnitures.includes(furniture)) {
+            this.furnitures.push(furniture);
+            this.onFurnituresChanged.run([...this.furnitures]);
+        }
+    }
+
+    removeFurniture(furniture) {
+        let removed = this.furnitures.remove(furniture);
+        if (removed) {
+            this.onFurnituresChanged.run([...this.furnitures]);
+        }
     }
 }
--- src/Utility.js
+++ src/Utility.js
@@ -102,7 +102,9 @@ Array.prototype.remove = function (value) {
     let index = this.indexOf(value);
     if (index >= 0) {
         this.splice(index, 1);
+        return true;
     }
+    return false;
 }
 
 /**
