76d9680cdebe6ea8eda8bad9c8c2d2b7087b4e3b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-06 22:23:42 -0500

    Add Block.js

---
+++ src/Block.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class Block{
+    constructor(width, length, height) {
+        this._width = width ?? 11;
+        this._length = length ?? 12;
+        this._height = height ?? 9;
+        this.units = "feet";
+        this.onSizeChanged = new Delegate("width", "length", "height");
+    }
+
+    get width() {
+        return this._width;
+    }
+    set width(value) {
+        this._width = value;
+        this.onSizeChanged.run(this._width, this._length, this._height);
+    }
+
+    get length() {
+        return this._length;
+    }
+    set length(value) {
+        this._length = value;
+        this.onSizeChanged.run(this._width, this._length, this._height);
+    }
+
+    get height() {
+        return this._height;
+    }
+    set height(value) {
+        this._height = value;
+        this.onSizeChanged.run(this._width, this._length, this._height);
+    }
+}
--- src/Room.js
+++ src/Room.js
@@ -1,35 +1,7 @@
 "use strict";
 
-class Room {
-    constructor(width, length, height) {
-        this._width = width ?? 11;
-        this._length = length ?? 12;
-        this._height = height ?? 9;
-        this.units = "feet";
-        this.onSizeChanged = new Delegate("width", "length", "height");
-    }
-
-    get width() {
-        return this._width;
-    }
-    set width(value) {
-        this._width = value;
-        this.onSizeChanged.run(this._width, this._length, this._height);
-    }
-
-    get length() {
-        return this._length;
-    }
-    set length(value) {
-        this._length = value;
-        this.onSizeChanged.run(this._width, this._length, this._height);
-    }
-
-    get height() {
-        return this._height;
-    }
-    set height(value) {
-        this._height = value;
-        this.onSizeChanged.run(this._width, this._length, this._height);
+class Room extends Block{
+    constructor() {
+        super();
     }
 }
