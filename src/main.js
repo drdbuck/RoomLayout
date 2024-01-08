b96bba7b45089cb205825750988c3e53132ffa3e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-07 22:45:50 -0500

    Add createFloor()

--- src/main.js
+++ src/main.js
@@ -208,3 +208,55 @@ function getDataStringify() {
         stringifyFurniture,
     ].flat();
 }
+
+function createFloor(width = 11, length = 12) {
+    //2024-01-07: copied from copied from https://github.com/mrdoob/three.js/blob/master/examples/misc_controls_pointerlock.html
+
+    const vertex = new Vector3();
+    const color = new Color();
+
+    // floor
+
+    let floorGeometry = new PlaneGeometry(
+        width,
+        length,
+        Math.ceil(width / 20),
+        Math.ceil(length / 20)
+    );
+    floorGeometry.rotateX( - Math.PI / 2 );
+
+    // vertex displacement
+
+    let position = floorGeometry.attributes.position;
+
+    for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+        vertex.fromBufferAttribute( position, i );
+
+        vertex.x += Math.random() * 20 - 10;
+        vertex.y += Math.random() * 2;
+        vertex.z += Math.random() * 20 - 10;
+
+        position.setXYZ( i, vertex.x, vertex.y, vertex.z );
+
+    }
+
+    floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+    position = floorGeometry.attributes.position;
+    const colorsFloor = [];
+
+    for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+        color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, SRGBColorSpace );
+        colorsFloor.push( color.r, color.g, color.b );
+
+    }
+
+    floorGeometry.setAttribute( 'color', new Float32BufferAttribute( colorsFloor, 3 ) );
+
+    const floorMaterial = new MeshBasicMaterial( { vertexColors: true } );
+
+    const floor = new Mesh( floorGeometry, floorMaterial );
+    return floor;
+}
