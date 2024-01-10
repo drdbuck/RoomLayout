f5ad3c74fd0aa4874f213e6eb007ec5e7fb06959
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-09 21:16:45 -0500

    Add LocalStorage.js

---
+++ src/LocalStorage.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const houseSaveKey = "RoomLayout_house";
+
+function saveHouse(house) {
+    let json = house;
+    if (!isString(json)) {
+        json = JSON.stringify(house, getDataStringify());
+    }
+    localStorage.setItem(houseSaveKey, house);
+}
+
+function loadHouse() {
+    let json = localStorage.getItem(houseSaveKey);
+    let house = JSON.parse(json);
+    if (!house) {
+        house = new House();
+    }
+    return house;
+}
