f4ede32b00e11a13397920d64ece38d56f5be2ab
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-09 19:42:48 -0500

    Autoformat

--- src/Constructor.js
+++ src/Constructor.js
@@ -29,7 +29,7 @@ function createFloor(width = 11, length = 12, showTriangles = false) {
         Math.ceil(width * 2),
         Math.ceil(length * 2)
     );
-    floorGeometry.rotateX( - Math.PI / 2 );
+    floorGeometry.rotateX(- Math.PI / 2);
 
     //color triangles
 
@@ -42,52 +42,52 @@ function createFloor(width = 11, length = 12, showTriangles = false) {
 
     const colorsFloor = [];
 
-    for ( let i = 0, l = position.count; i < l; i ++ ) {
+    for (let i = 0, l = position.count; i < l; i++) {
 
-        let shade = Math.random() * 0.05 +0.5;
+        let shade = Math.random() * 0.05 + 0.5;
         color.setRGB(shade, shade, shade, SRGBColorSpace);
-        colorsFloor.push( color.r, color.g, color.b );
+        colorsFloor.push(color.r, color.g, color.b);
 
     }
 
-    floorGeometry.setAttribute( 'color', new Float32BufferAttribute( colorsFloor, 3 ) );
+    floorGeometry.setAttribute('color', new Float32BufferAttribute(colorsFloor, 3));
 
     //floor material
-    const floorMaterial = new MeshBasicMaterial( { vertexColors: true } );
+    const floorMaterial = new MeshBasicMaterial({ vertexColors: true });
 
     //floor mesh
-    const floor = new Mesh( floorGeometry, floorMaterial );
+    const floor = new Mesh(floorGeometry, floorMaterial);
     return floor;
 }
 
 function constructFurniture(furniture) {
     //2024-01-09: copied from https://github.com/mrdoob/three.js/blob/master/examples/misc_controls_pointerlock.html
-    const boxGeometry = new THREE.BoxGeometry( 20, 20, 20 ).toNonIndexed();
+    const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
 
-				position = boxGeometry.attributes.position;
-				const colorsBox = [];
+    position = boxGeometry.attributes.position;
+    const colorsBox = [];
 
-				for ( let i = 0, l = position.count; i < l; i ++ ) {
+    for (let i = 0, l = position.count; i < l; i++) {
 
-					color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace );
-					colorsBox.push( color.r, color.g, color.b );
+        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
+        colorsBox.push(color.r, color.g, color.b);
 
-				}
+    }
 
-				boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
+    boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
 
-				for ( let i = 0; i < 500; i ++ ) {
+    for (let i = 0; i < 500; i++) {
 
-					const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
-					boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace );
+        const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
+        boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
 
-					const box = new THREE.Mesh( boxGeometry, boxMaterial );
-					box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-					box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
-					box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+        const box = new THREE.Mesh(boxGeometry, boxMaterial);
+        box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
+        box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
+        box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
 
-					scene.add( box );
-					objects.push( box );
+        scene.add(box);
+        objects.push(box);
 
-				}
+    }
 }
