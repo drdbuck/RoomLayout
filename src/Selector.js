d6529e8da8f9d5ff773da36937562c80d72af967
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-12 22:57:54 -0500

    Modify Selector.js

--- src/Selector.js
+++ src/Selector.js
@@ -4,13 +4,21 @@
  * Class for selecting groups of objects.
  * Meant to work with groups of objects, not groups of primitives or groups of arrays
  */
-class Selector{
+class Selector {
     constructor() {
-        this.selection = [];
+        this._selection = [];
 
         this.onSelectionChanged = new Delegate("selection");
         this.onSelectionLost = new Delegate("oldselect");
         this.onSelectionGained = new Delegate("newselect");
+    }
+
+    get selection() {
+        return this._selection.slice();
+    }
+
+    get count() {
+        return this._selection.length;
     }
 
     /**
@@ -18,10 +26,10 @@ class Selector{
      * @param {object} item The item to select
      */
     select(item) {
-        if (!this.selection.includes(item)) {
-            this.selection.push(item);
+        if (!this._selection.includes(item)) {
+            this._selection.push(item);
             this.onSelectionGained.run(item);
-            this.onSelectionChanged.run(this.selection);
+            this.onSelectionChanged.run(this._selection);
         }
     }
 
@@ -31,16 +39,33 @@ class Selector{
      */
     selectAll(...items) {
         items = items.flat();
-        let oldlength = this.selection.length;
+        let oldlength = this._selection.length;
         for (let item of items) {
-            if (!this.selection.includes(item)) {
-                this.selection.push(item);
+            if (!this._selection.includes(item)) {
+                this._selection.push(item);
                 this.onSelectionGained.run(item);
             }
         }
-        if (this.selection.length != oldlength) {
-            this.onSelectionChanged.run(this.selection);
+        if (this._selection.length != oldlength) {
+            this.onSelectionChanged.run(this._selection);
         }
+    }
+
+    /**
+     * Selects the given item, deselecting all other selected items first
+     * @param {object} item The object to select
+     */
+    selectOnly(item) {
+        if (!item) { return; }
+        //clear
+        for (let item of this._selection) {
+            this.onSelectionLost.run(item);
+        }
+        this._selection.length = 0;
+        //select
+        this._selection.push(item);
+        this.onSelectionGained.run(item);
+        this.onSelectionChanged.run(this._selection);
     }
 
     /**
@@ -48,10 +73,10 @@ class Selector{
      * @param {object} item The object to deselect
      */
     deselect(item) {
-        let removed = this.selection.remove(item);
+        let removed = this._selection.remove(item);
         if (removed) {
             this.onSelectionLost.run(item);
-            this.onSelectionChanged.run(this.selection);
+            this.onSelectionChanged.run(this._selection);
         }
     }
 
@@ -59,10 +84,23 @@ class Selector{
      * Deselect all the selected items
      */
     clear() {
-        for (let item of this.selection) {
+        for (let item of this._selection) {
             this.onSelectionLost.run(item);
         }
-        this.selection.length = 0;
-        this.onSelectionChanged.run(this.selection);
+        this._selection.length = 0;
+        this.onSelectionChanged.run(this._selection);
+    }
+
+    /**
+     * Runs the given function over all the selected items
+     * @param {(item:object)=>void} func The function that will process the selected items
+     */
+    forEach(func) {
+        if (!func) {
+            console.error("Missing func!", func);
+            return;
+        }
+        //
+        this._selection.forEach(func);
     }
 }
