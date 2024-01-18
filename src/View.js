cd6d96627701ceead1fc4c6ed5dd15e737ef357a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-01-18 16:34:34 -0500

    Add View.js

---
+++ src/View.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class View{
+    constructor(position, quaternion) {
+        this._position = position;
+        this._quaternion = quaternion;
+    }
+
+    get position() {
+        return this._position;
+    }
+    set position(value) {
+        this._position.copy(value);
+    }
+
+    get quaternion() {
+        return this._quaternion;
+    }
+    set quaternion(value) {
+        this._quaternion.copy(value);
+    }
+}
