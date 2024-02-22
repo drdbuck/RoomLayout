15525ab82cc558952394e5386e952b32db66dd19
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-02-21 19:06:43 -0500

    Groups  set their own position / rotation / scale

--- src/Data/KitBash.js
+++ src/Data/KitBash.js
@@ -12,6 +12,29 @@ class KitBash extends Block {
 
         this._items = [];
         items.filter(i => i).forEach(item => this.add(item));
+
+        //Position
+        let pos = _zero.clone();
+        let sumX = this._items.sum(f => f.position.x);
+        pos.x = sumX / this._items.length;
+        let sumZ = this._items.sum(f => f.position.z);
+        pos.z = sumZ / this._items.length;
+        let minY = this._items.min(f => f.altitude);
+        pos.y = minY;
+        this._position.copy(pos);
+
+        //Rotation
+        let sumAngle = this._items.sum(f => loopAngle(f.angle));
+        this._angle = sumAngle / this._items.length;
+
+        //Scale
+        //TODO: account for rotation of individual parts
+        let maxHalfWidth = this._items.max(f => pos.distanceTo(f.position) + (f.width / 2));
+        let maxHalfDepth = this._items.max(f => pos.distanceTo(f.position) + (f.depth / 2));
+
+        //Delegates
+        this.onItemAdded = new Delegate("item");
+        this.onItemRemoved = new Delegate("item");
     }
 
     get items() {
@@ -48,6 +71,155 @@ class KitBash extends Block {
 
     has(item) {
         return this._items.includes(item);
+    }
+
+    //Position
+    get position() {
+        let pos = new Vector2();
+        let sumX = this._items.sum(f => f.position.x);
+        pos.x = sumX / this._items.length;
+        let sumZ = this._items.sum(f => f.position.z);
+        pos.z = sumZ / this._items.length;
+        let minY = this._items.min(f => f.altitude);
+        pos.y = minY;
+        return pos;
+    }
+    set position(value) {
+        //get offset
+        const oldPos = this.position;
+        let offset = value.clone();
+        offset.subtract(oldPos);
+        //offset each individual piece of furniture
+        this._items.forEach(item => {
+            let newPos = item.position.clone();
+            newPos.add(offset);
+            item.position = newPos;
+        });
+        //delegate
+        super.position = value;
+    }
+
+    //Rotation
+    get angle() {
+        let sumAngle = this._items.sum(f => loopAngle(f.angle));
+        return sumAngle / this._items.length;
+    }
+    set angle(value) {
+        const oldAngle = this.angle;
+        let offset = n - oldAngle;
+        const center = this.position;
+        this._items.forEach(item => {
+            //update angle
+            item.angle += offset;
+            //update position
+            let poffset = item.position.clone();
+            poffset.sub(c.center);
+            let radians = toRadians(offset);
+            poffset.applyAxisAngle(_up, radians);
+            poffset.add(c.center);
+            item.position = poffset;
+        });
+        super.angle = value;
+    }
+
+    //Scale
+    get scale() {
+        let width = this.width;
+        let depth = this.depth;
+        let height = this.height;
+        return new Vector2(width, height, depth);
+    }
+    set scale(value) {
+        this.width = value.x;
+        this.depth = value.z;
+        this.height = value.y;
+        //
+        super.scale = value;
+    }
+
+    get width() {
+        //TODO: calculate size
+        return this._items[0].width;
+    }
+    set width(value) {
+        const oldWidth = this.width;
+        const factor = value / oldWidth;
+        const center = this.position;
+        this._items.forEach(item => {
+            //adjust dimensions
+            item.width *= factor;
+            //move
+            //TODO: account for rotation
+            let offset = item.position.clone();
+            offset.subtract(center);
+            offset.x *= factor;
+            offset.add(center);
+            item.position = offset;
+        });
+        super.width = value;
+    }
+
+    get depth() {
+        //TODO: calculate size
+        return this._items[0].depth;
+    }
+    set depth(value) {
+        const prev = this.depth;
+        let factor = value / prev;
+        this._items.forEach(item => {
+            //adjust dimensions
+            item.depth *= factor;
+            //move
+            //TODO: account for rotation
+            let offset = item.position.clone();
+            offset.subtract(center);
+            offset.z *= factor;
+            offset.add(center);
+            item.position = offset;
+        });
+        super.depth = value;
+    }
+
+    get length() {
+        //TODO: calculate size
+        return this._items[0].length;
+    }
+    set length(value) {
+        const prev = this.length;
+        let factor = value / prev;
+        this._items.forEach(item => {
+            //adjust dimensions
+            item.length *= factor;
+            //move
+            //TODO: account for rotation
+            let offset = item.position.clone();
+            offset.subtract(center);
+            offset.z *= factor;
+            offset.add(center);
+            item.position = offset;
+        });
+        super.length = value;
+    }
+
+    get height() {
+        //TODO: account for rotation / recline
+        return this._items.max(f => f.altitude + f.height) - this.altitude;
+    }
+    set height(value) {
+        const prev = this.height;
+        let factor = value / prev;
+        this._items.forEach(item => {
+            //adjust dimensions
+            item.height *= factor;
+            //move
+            //TODO: account for rotation / recline
+            let offset = item.position.clone();
+            offset.subtract(center);
+            offset.y *= factor;
+            offset.add(center);
+            item.position = offset;
+        });
+        super.height = value;
     }
 
 }
