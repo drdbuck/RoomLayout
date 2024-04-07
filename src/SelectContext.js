6a20237fd06fe154e58632890507afe16596a11d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-04-06 20:57:21 -0400

    #6 Add SelectContext

---
+++ src/SelectContext.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const stringifySelectContext = [
+    "uid_obj",
+    "uid_furniture",
+    "uid_kitbash",
+    "face",
+]
+
+class SelectContext {
+    constructor(select, face = -2) {
+        this.obj = select;
+        this.furniture = undefined;
+        this.kitbash = undefined;
+        this.box = undefined;
+        this.boxes = undefined;
+        this.face = face;
+        this.offset = _zero.clone();
+    }
+
+    grabInfo() {
+        this.grabIds();
+        this.grabBoxes();
+    }
+
+    grabIds() {
+        this.uid_obj = this.obj.uid;
+        this.uid_furniture = this.furniture?.uid;
+        this.uid_kitbash = this.kitbash?.uid;
+    }
+
+    grabBoxes() {
+        if (this.obj) {
+            this.box = getBox(this.obj);
+        }
+        if (this.furniture) {
+            this.box = getBox(this.furniture) ?? this.box;
+        }
+        if (this.kitbash) {
+            this.boxes = getBoxes(this.kitbash.items);
+        }
+        this.box ??= this.boxes?.[0];
+        this.boxes ??= [this.box];
+
+        return this.box && this.boxes?.length > 0;
+    }
+
+    inflate() {
+        if (this.uid_obj) {
+            let uid = this.uid_obj;
+            let obj = uiVars.findUid(house, uid);
+            this.obj = obj;
+        }
+        if (this.uid_furniture) {
+            let uid = this.uid_furniture;
+            let obj = uiVars.findUid(house, uid);
+            this.furniture = obj;
+        }
+        if (this.uid_kitbash) {
+            let uid = this.uid_kitbash;
+            let obj = uiVars.findUid(house, uid);
+            this.kitbash = obj;
+        }
+        //
+        this.offset = _zero.clone();
+        //
+        let boxesGrabbed = this.grabBoxes();
+        //
+        return this.obj && boxesGrabbed;
+    }
+}
+
+function inflateSelectContext(context){
+    inflateObject(context, SelectContext.prototype);
+    return context.inflate();
+}
