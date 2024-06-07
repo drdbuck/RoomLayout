e7d687845726a3f7026700e86222c78b597aa388
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-06-07 18:22:20 -0400

    #18 fix bug: default face getting selected first

--- src/Controller.js
+++ src/Controller.js
@@ -160,7 +160,7 @@ class Controller {
                     let context = this.selectObject(
                         selectTarget,
                         this.multiselectButton,
-                        (selectTarget.isKitBash) ? FACE_DEFAULT : targetFace,
+                        targetFace,
                         !onlySelectButton && !anyPieceSingleSelected
                     );
                     context.stable = false;
